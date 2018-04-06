caf92ac742913e81e667e285bf3e5b442615236a
unknown <WXP22@corp.bl.com>
2018-04-06 09:17:05 +0800

    iwantrun admin

---
+++ iwant-admin/src/main/java/com/iwantrun/admin/IwantAdminApplication.java
@@ -0,0 +1,12 @@
+package com.iwantrun.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IwantAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IwantAdminApplication.class, args);
+	}
+}
