af3e09d4cada4523742cd6bbfb8c7214e9f1bdff
WXP22 <WXP22@LQDEV022.corp.bl.com>
2018-04-06 10:43:14 +0800

    login controller

---
+++ iwant-admin/src/main/java/com/iwantrun/admin/controller/LoginPageController.java
@@ -0,0 +1,13 @@
+package com.iwantrun.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginPageController {
+	
+	@RequestMapping("/login")
+	public String loginPage() {
+		return "login";
+	}
+}
