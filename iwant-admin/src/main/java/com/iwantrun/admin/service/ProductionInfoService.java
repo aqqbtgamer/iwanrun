588e47fc2a609f407a700163bf3101bd202a8798
dawn <2915755767@qq.com>
2018-04-22 13:57:36 +0800

    create location and query perfect

--- iwant-admin/src/main/java/com/iwantrun/admin/service/ProductionInfoService.java
+++ iwant-admin/src/main/java/com/iwantrun/admin/service/ProductionInfoService.java
@@ -1,18 +1,27 @@
 package com.iwantrun.admin.service;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.iwantrun.admin.transfer.Message;
+import com.iwantrun.admin.utils.CookieUtils;
+import com.iwantrun.admin.utils.FormDataUtils;
 
 import net.minidev.json.JSONArray;
 import net.minidev.json.JSONObject;
 
 @Service
 public class ProductionInfoService {
+	@Autowired  
+    private Environment env;  
+	
 	@Autowired
 	private RestTemplate template;
 
@@ -21,7 +30,31 @@ public class ProductionInfoService {
 	}
 
 	public String add(HttpServletRequest request) {
-		return null;
+		String token = CookieUtils.getLoginToken(request);
+		List<String> paramList = FormDataUtils.stringArray2List(new String[] {
+				"name",
+				"activity_type_code",
+				"during",
+				"group_number",
+				"priority",
+				"activity_province_code",
+				"activity_city_code",
+				"activity_dist_code",
+				"descirbeText1",
+				"descirbeText2",
+				"descirbeText3",
+				"mainImage",
+				"_ue"
+		});
+		String json = FormDataUtils.formData2Json(request,paramList);
+		String postUrl = env.getProperty("application.location.add");
+		String baseUrl = env.getProperty("application.serverbase");
+		Message message = new Message();
+		message.setAccessToken(token);
+		message.setMessageBody(json);
+		message.setRequestMethod(baseUrl+postUrl);
+		ResponseEntity<Message> response = template.postForEntity(baseUrl+postUrl, message, Message.class);		
+		return response == null ? null : response.getBody().getMessageBody();
 	}
 
 	public String find(HttpServletRequest request) {
@@ -49,11 +82,6 @@ public class ProductionInfoService {
 		message.setMessageBody(param.toJSONString());
 		
 		System.err.println("请求====="+param.toJSONString());
-		
-		/*JSONArray arr = template.postForEntity("http://127.0.0.1:9999/iwant_app/application/productionInfo/find",
-				message, JSONArray.class).getBody();
-
-		System.err.println("响应====="+arr.toJSONString());*/
 		
 		JSONObject obj = template.postForEntity("http://127.0.0.1:9999/iwant_app/application/productionInfo/find",
 				message, JSONObject.class).getBody();
---
+++ iwant-admin/src/main/resources/static/js/js_dev/productionInfoCreate.app.js
@@ -0,0 +1,43 @@
+/**
+ * Created by WXP22 on 2018/4/14.
+ */
+
+var uploadServer = '/iwant_admin/remote/fileupload';
+var submitUrl = '/iwant_admin/location/add'
+
+$(document).ready(
+    function(){
+        //initUE();
+        bindUploadFile('mainImageUpload',uploadServer,'mainImage',singleDisplay);
+        bindUploadFile('uploadedLocationImages',uploadServer,'imgManage',mutipleDisplay);
+        bindDataSubmit('submitForm',
+            new Array("name",
+    				"activity_type_code",
+    				"during",
+    				"group_number",
+    				"priority",
+    				"activity_province_code",
+    				"activity_city_code",
+    				"activity_dist_code",
+    				"descirbeText1",
+    				"descirbeText2",
+    				"descirbeText3",
+    				"mainImage"
+            ),
+            submitUrl,returnListPage);
+        bindSeclectAll("checkAll");
+        bindDeleteSelected("deleteAll");
+    }
+);
+
+function returnListPage(result){
+	if(result == "failed"){
+		alert("后台处理数据失败")
+	}else{
+		window.location.href="locationlist.html";
+	}
+}
+
+
+
+
