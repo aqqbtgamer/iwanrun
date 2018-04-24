4c38ed66b85d911351e1fa2b7fd8e63c0e5d3c4c
aqqbtgamer <30305701+aqqbtgamer@users.noreply.github.com>
2018-04-24 12:55:18 +0800

    commit for luban

---
+++ ibailian-luban/src/main/java/com/ibailian/admin/RuBanApplication.java
@@ -0,0 +1,12 @@
+package com.ibailian.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuBanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuBanApplication.class, args);
+	}
+}
