080bde7f62444035335e16495977e5b35942a096
aqqbtgamer <30305701+aqqbtgamer@users.noreply.github.com>
2018-04-24 13:05:26 +0800

    ffr  han

--- ibailian-luban/src/main/java/com/ibailian/admin/RuBanApplication.java
+++ ibailian-luban/src/main/java/com/ibailian/admin/RuBanApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RuBanApplication {
 
 	//added by han
+	
 	public static void main(String[] args) {
 		SpringApplication.run(RuBanApplication.class, args);
 	}
