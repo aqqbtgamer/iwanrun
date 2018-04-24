c9d0ad705f55f29c522ebaa3d428d8bd5af77a56
yanghan3720 <1366898389@qq.com>
2018-04-24 13:14:48 +0800

    han

--- ibailian-luban/src/main/java/com/ibailian/admin/RuBanApplication.java
+++ ibailian-luban/src/main/java/com/ibailian/admin/RuBanApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class RuBanApplication {
 
 	//added by han
-	//123
+	//123  123
 	public static void main(String[] args) {
 		SpringApplication.run(RuBanApplication.class, args);
 	}
