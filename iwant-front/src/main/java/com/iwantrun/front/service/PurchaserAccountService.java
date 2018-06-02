b5102b5c57b6f309272409492d41ca31554e9fdf
dawn <2915755767@qq.com>
2018-06-02 17:37:05 +0800

    短信验证码

--- iwant-front/src/main/java/com/iwantrun/front/service/PurchaserAccountService.java
+++ iwant-front/src/main/java/com/iwantrun/front/service/PurchaserAccountService.java
@@ -14,6 +14,7 @@ import com.iwantrun.front.utils.JSONUtils;
 public class PurchaserAccountService {
 	@Autowired
 	private Environment environment;
+	@Autowired
 	private RestTemplate template;
 
 	/**
