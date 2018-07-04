592139b07ee9ff996a0dee50ec053d11315f01c9
aqqbtgamer <30305701+aqqbtgamer@users.noreply.github.com>
2018-07-04 15:49:27 +0800

    test class add

---
+++ application/src/test/java/com/iwantrun/core/service/application/domain/LocationTagsTest.java
@@ -0,0 +1,34 @@
+package com.iwantrun.core.service.application.domain;
+
+import java.beans.PropertyDescriptor;
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.beanutils.PropertyUtils;
+import org.junit.Test;
+
+public class LocationTagsTest {
+
+	@Test
+	public void testPropertyUtils() throws IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+		LocationTags tags = new LocationTags();
+		//OrderHistory history = new OrderHistory();
+		PropertyUtils.setProperty(tags, "tagsType", 1);
+		//PropertyUtils.setProperty(history, "orderId", 1);
+		/*PropertyDescriptor[] desc = PropertyUtils.getPropertyDescriptors(LocationTags.class);
+		for(PropertyDescriptor descri : desc) {
+			try {
+				System.out.println(descri.getName());
+				System.out.println(descri.getReadMethod().getName());
+				System.out.println(descri.getWriteMethod().getName());
+			}catch(Exception e) {
+				
+			}
+			
+		}*/
+		//BeanUtils.setProperty(tags, "tagsType", 2);
+		//tags.setTagsType(2);
+		System.out.println(tags.getTagsType());
+	}
+
+}
