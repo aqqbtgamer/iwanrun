20ad7ccd73440ec3e7f90708fe03cf5b8acc2ada
dawn <2915755767@qq.com>
2018-07-22 22:27:01 +0800

    Purchaser Account Info Perfect

--- iwant-front/src/main/java/com/iwantrun/front/constants/CookieConstants.java
+++ iwant-front/src/main/java/com/iwantrun/front/constants/CookieConstants.java
@@ -5,6 +5,12 @@ public interface CookieConstants {
 	 * 配置文件里面的AccessToken有效期的key值
 	 */
 	String COOKIE_EXPIRY_ACCESS_TOKEN_KEY = "accessToken.cookie.expiry";
+
+	/**
+	 * 配置文件里面的临时AccessToken有效期的key值
+	 */
+	String COOKIE_TEMP_EXPIRY_ACCESS_TOKEN_KEY = "accessToken.temp.cookie.expiry";
+
 	/**
 	 * 存入cookie的key值
 	 */
