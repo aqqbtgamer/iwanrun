f3503f34af06f4f74eeb75a64cac33b05e90574c
aqqbtgamer <30305701+aqqbtgamer@users.noreply.github.com>
2018-08-13 09:58:38 +0800

    fix compile issue

--- application/src/main/java/com/iwantrun/core/service/application/service/OrdersService.java
+++ application/src/main/java/com/iwantrun/core/service/application/service/OrdersService.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import org.mockito.internal.util.StringUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
