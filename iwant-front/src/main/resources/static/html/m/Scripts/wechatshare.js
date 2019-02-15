27ff3e2fa66f41da915b71a26626e1265c4ce053
thomerson <tanghongsheng123@126.com>
2019-02-15 20:51:49 +0800

    update

--- iwant-front/src/main/resources/static/html/m/Scripts/wechatshare.js
+++ iwant-front/src/main/resources/static/html/m/Scripts/wechatshare.js
@@ -6,10 +6,9 @@
 
     var url = requestUrl.getWeixingConfig;
     is_weixn() && wx && $.getJSON(url + '?url=' + location.href.split('#')[0], function (data) {
-        if (data && data.ErrMsg) {
-            console.log(data.ErrMsg);
-            console.log(data.StackTrace);
+        if (data && !data.signature) {
             return false;
+            console.log('签名失败');
         }
         var config = {
             title: '我要跑',
@@ -27,7 +26,7 @@
             //若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
             appId: data.appId, // 必填，公众号的唯一标识
             timestamp: data.timestamp, // 必填，生成签名的时间戳
-            nonceStr: data.nonce, // 必填，生成签名的随机串
+            nonceStr: data.nonceStr, // 必填，生成签名的随机串
             signature: data.signature,// 必填，签名，见附录1
             jsApiList: config.jsApiList // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
         });
